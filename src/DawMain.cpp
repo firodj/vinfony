3d5aa6dd5aa0261834bf4a09ee52bbccaa9e0bd6
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-05-18 15:33:51 +0700

    Draw Timeline and Cursor

--- src/DawMain.cpp
+++ src/DawMain.cpp
@@ -6,6 +6,14 @@
 #include <imgui.h>
 #include <imgui_internal.h>
 
+namespace ImGui {
+  ImVec2 GetScroll()
+  {
+      ImGuiWindow* window = GImGui->CurrentWindow;
+      return window->Scroll;
+  }
+};
+
 namespace vinfony {
   const int SplitterThickness = 8;
 
@@ -32,7 +40,8 @@ namespace vinfony {
     auto track = storage.tracks[storage.last_tracks_id].get();
     track->id = storage.last_tracks_id;
     track->name = name;
-    track->h = 20;
+    float ht = (float)(((int)ImGui::GetFrameHeightWithSpacing()*3/2) & ~1);
+    track->h = ht;
 
     storage.track_nums.push_back(track->id);
 
@@ -60,7 +69,7 @@ namespace vinfony {
     NewProp(storage, "Name", [](DawPropDrawParam * param) {
       auto p1 = ImGui::GetCursorScreenPos();
       auto p2 = p1 + ImVec2{(float)param->self->w, (float)param->track->h};
-      ImGui::PushClipRect(p1, p2, false);
+      ImGui::PushClipRect(p1, p2, true);
       // ImGui::GetWindowDrawList()->AddRectFilled(p1, p2, IM_COL32(255, 0,0, 255));
       ImGui::Text("%s", param->track->name.c_str() );
       ImGui::PopClipRect();
@@ -132,13 +141,13 @@ namespace vinfony {
 
 #endif
 
-  static void VSplitter(float pos_x, float pos_y, float avail_h, SplitterOnDraggingFunc func) {
+  static void VSplitter(ImVec2 pos, float avail_h, SplitterOnDraggingFunc func) {
     auto draw_list     = ImGui::GetWindowDrawList();
     ImU32 color_border = ImGui::GetColorU32(ImGuiCol_Separator, 1.0);
     ImU32 color_hover  = ImGui::GetColorU32(ImGuiCol_SeparatorHovered, 1.0);
     ImU32 color_active  = ImGui::GetColorU32(ImGuiCol_SeparatorActive, 1.0);
 
-    ImGui::SetCursorPos({ pos_x, pos_y });
+    ImGui::SetCursorPos(pos);
     ImGui::InvisibleButton("vsplitter", ImVec2{SplitterThickness, (float)ImMax(avail_h, 1.0f)});
     auto rcmin = ImGui::GetItemRectMin();
     auto rcmax = ImGui::GetItemRectMax();
@@ -155,13 +164,13 @@ namespace vinfony {
     draw_list->AddLine(rcmin, rcmax, color_border);
   }
 
-  static void HSplitter(float pos_x, float pos_y, float avail_w, SplitterOnDraggingFunc func) {
+  static void HSplitter(ImVec2 pos, float avail_w, SplitterOnDraggingFunc func) {
     auto draw_list     = ImGui::GetWindowDrawList();
     ImU32 color_border = ImGui::GetColorU32(ImGuiCol_Separator, 1.0);
     ImU32 color_hover  = ImGui::GetColorU32(ImGuiCol_SeparatorHovered, 1.0);
     ImU32 color_active = ImGui::GetColorU32(ImGuiCol_SeparatorActive, 1.0);
 
-    ImGui::SetCursorPos({pos_x, pos_y});
+    ImGui::SetCursorPos(pos);
     ImGui::InvisibleButton("hsplitter", ImVec2{(float)ImMax(avail_w, 1.0f), SplitterThickness});
     auto rcmin = ImGui::GetItemRectMin();
     auto rcmax = ImGui::GetItemRectMax();
@@ -192,13 +201,15 @@ namespace vinfony {
     DawMainStorage &storage = g_storages[*storageIdx];
 
     static float w = 200.0f;
+    float h0 = (float)(((int)ImGui::GetFrameHeightWithSpacing()*3/2) & ~1);
 
     ImU32 color_border = ImGui::GetColorU32(ImGuiCol_Separator, 1.0);
     // DAW test
     auto conavail = ImGui::GetContentRegionAvail();
 
-    //ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
-
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{0, 0});
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{0, 0});
+    ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32_WHITE);
     ImGui::SetNextWindowScroll(ImVec2{storage.scroll_x0, storage.scroll_y});
     if (ImGui::BeginChild("child_1", {w, 0.0f}, ImGuiChildFlags_ResizeX | ImGuiChildFlags_Border, ImGuiWindowFlags_HorizontalScrollbar)) {
       auto draw_list = ImGui::GetWindowDrawList();
@@ -210,18 +221,18 @@ namespace vinfony {
       auto avail = ImGui::GetContentRegionAvail();
 
       // Row 0: Header
-      int pos_x = xy0.x;
+      float pos_x = xy0.x;
       for (int c=0; c<storage.prop_nums.size(); c++) {
         int prop_id = storage.prop_nums[c];
         DawProp * prop = storage.props[prop_id].get();
         if (c > 0) ImGui::SameLine();
         ImGui::SetCursorPosX(pos_x);
-        ImGui::Button(prop->name.c_str(), {(float)prop->w, 0});
+        ImGui::Button(prop->name.c_str(), {(float)prop->w, h0});
         pos_x += prop->w + SplitterThickness;
       }
 
       // Row N: Track
-      int pos_y = xy0.y + ImGui::GetFrameHeightWithSpacing();
+      float pos_y = xy0.y + h0;
       for (int r=0; r<storage.track_nums.size(); r++) {
         int track_id = storage.track_nums[r];
         DawTrack * track = storage.tracks[track_id].get();
@@ -237,6 +248,7 @@ namespace vinfony {
           param.track = track;
           param.r = r;
           param.c = c;
+
           if (prop->DrawTrack) {
             if (c > 0) ImGui::SameLine();
             ImGui::SetCursorPosX(pos_x);
@@ -259,20 +271,20 @@ namespace vinfony {
         DawProp * prop = storage.props[prop_id].get();
         pos_x += prop->w;
         ImGui::PushID(c);
-        VSplitter(pos_x, pos_y, avail.y, [&]() { prop->w = ImMax(prop->w + ImGui::GetIO().MouseDelta.x, 20.0f); });
+        VSplitter({pos_x, pos_y}, avail.y, [&]() { prop->w = ImMax(prop->w + ImGui::GetIO().MouseDelta.x, 20.0f); });
         ImGui::PopID();
         pos_x += SplitterThickness;
       }
 
-      // Borders R
+      // Borders R-L
       pos_x = xy0.x;
-      pos_y = xy0.y + ImGui::GetFrameHeightWithSpacing();
+      pos_y = xy0.y + h0;
       for (int r=0; r<storage.track_nums.size(); r++) {
         int track_id = storage.track_nums[r];
         DawTrack * track = storage.tracks[track_id].get();
         pos_y += track->h;
         ImGui::PushID(r);
-        HSplitter(pos_x, pos_y, avail.x, [&]() { track->h = ImMax(track->h + ImGui::GetIO().MouseDelta.y, 20.0f); });
+        HSplitter({pos_x, pos_y}, avail.x, [&]() { track->h = ImMax(track->h + ImGui::GetIO().MouseDelta.y, 20.0f); });
         ImGui::PopID();
         pos_y += SplitterThickness;
       }
@@ -282,55 +294,108 @@ namespace vinfony {
       storage.scroll_x0 = ImGui::GetScrollX();
     }
     ImGui::EndChild();
+    ImGui::PopStyleColor();
+    ImGui::PopStyleVar(2);
 
     ImGui::SameLine(0, 0);
-  #if 0 // WITHOUT  SPLITTER
-    {
-      auto drawlist = ImGui::GetWindowDrawList();
-      auto cursor = ImGui::GetCursorScreenPos();
-      ImGui::InvisibleButton("v_splitter", {8.0, (float)ImMax(ImGui::GetWindowSize().y - ImGui::GetCursorPosY() - 10.0, 1.0)});
-      if (ImGui::IsItemHovered()) {
-        //if (ImGui::IsMouseClicked(ImGuiMouseButton_Left)) {
-        ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW);
-      }
-      if(ImGui::IsItemActive()) {
-          w += ImGui::GetIO().MouseDelta.x;
-      }
-      auto button = ImGui::GetItemRectSize();
-      auto rcmax = ImGui::GetItemRectMax(); // = cursor+button ?
-      drawlist->AddRect(cursor, cursor+button, IM_COL32(255, 0,0, 255));
-    }
-    ImGui::SameLine();
-  #endif
 
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2{0, 0});
+    ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{0, 0});
+    ImGui::PushStyleColor(ImGuiCol_ChildBg, IM_COL32_WHITE);
     ImGui::SetNextWindowScroll(ImVec2{storage.scroll_x1, storage.scroll_y});
     if (ImGui::BeginChild("child_2", {0, 0}, true, ImGuiWindowFlags_HorizontalScrollbar)) {
       auto draw_list = ImGui::GetWindowDrawList();
-      auto cursor = ImGui::GetCursorScreenPos();
+      auto timeline_pos = ImGui::GetCursorScreenPos();
       auto xy0 = ImGui::GetCursorPos();
       auto avail = ImGui::GetContentRegionAvail();
 
-      // Borders R
-      int pos_x = xy0.x;
-      int pos_y = xy0.y + ImGui::GetFrameHeightWithSpacing();
+      ImGui::SetCursorPos({1500, 1500});
+      ImGui::Dummy(ImVec2{1,1});
+      ImGui::SetCursorPos(xy0);
+
+      // ImGui::GetCursorScreenPos -> respect  scrolled  because translated from GetCursorPos.
+      // ImGui::GetContentRegionAvail -> a viewable portion only, dontcare scrolled.
+      // ImGui::GetWindowContentRegionMin -> (local) ImGui::GetCursorPos initially.
+      // ImGui::GetWindowContentRegionMax = ImGui::GetContentRegionMax ? -> respect scrolled
+      //
+
+      // Timeline
+
+      auto wndpos = ImGui::GetWindowPos();
+      auto wndsz = ImGui::GetWindowSize();
+#if false
+      auto sticky_p = wndpos; // OR timeline_pos + ImGui::GetScroll();
+      draw_list->AddRectFilled(sticky_p, sticky_p + ImVec2(wndsz.x, h0), IM_COL32(255,255,0,255));
+      ImGui::SetCursorScreenPos(sticky_p + ImVec2{4,4});
+      ImGui::Text("wndpos %.2f %.2f, wndsz %.2f %.2f", wndpos.x, wndpos.y, wndsz.x, wndsz.y);
+#endif
+      int t = 0;
+      int wt = 40;
+
+      ImVec2 scrnpos = timeline_pos; // + ImVec2{(float)wt - ((int)ImGui::GetScrollX() % wt), 0.0f};
+      ImVec2 scrnmax = wndpos + wndsz;
+      draw_list->AddLine({ timeline_pos.x, timeline_pos.y+h0/2}, ImVec2{scrnmax.x, timeline_pos.y+h0/2}, IM_COL32(255,128,0,255));
+      while (scrnpos.x < scrnmax.x) {
+        draw_list->AddLine(scrnpos + ImVec2{0, h0/2}, scrnpos + ImVec2{0, h0}, IM_COL32(255,128,0,255));
+        if (t % 4 == 0) {
+          draw_list->AddLine(scrnpos, scrnpos + ImVec2(0, h0/2), IM_COL32(255,128,0,255));
+          char tmps[512];
+          ImFormatString(tmps, IM_ARRAYSIZE(tmps), "%d", t);
+          draw_list->AddText(scrnpos, IM_COL32_BLACK, tmps);
+        }
+        scrnpos.x += wt;
+        t++;
+      }
+
+      // Cursor
+      {
+        int cursor_wd = 8;
+        static float cursor_x = wt * 2.0f;
+        ImGui::SetCursorPos({ cursor_x - (cursor_wd/2), 0.0f});
+        ImGui::InvisibleButton("cursor", ImVec2{(float)cursor_wd, h0});
+        auto rcmin = ImGui::GetItemRectMin();
+        auto rcmax = ImGui::GetItemRectMax();
+        if (ImGui::IsItemHovered()) {
+          ImGui::SetMouseCursor(ImGuiMouseCursor_ResizeEW);
+          // color_border = color_hover;
+        }
+        if(ImGui::IsItemActive() && ImGui::IsMouseDragging(ImGuiMouseButton_Left)) {
+          cursor_x = ImClamp(cursor_x + ImGui::GetIO().MouseDelta.x, 0.0f, 1500.0f);
+          // color_border = color_active;
+        }
+
+        ImVec2 points[] = {
+          { rcmin.x, rcmin.y},
+          { rcmin.x, rcmax.y - (cursor_wd/2)},
+          { rcmin.x + (cursor_wd/2), rcmax.y},
+          { rcmax.x, rcmax.y - (cursor_wd/2)},
+          { rcmax.x, rcmin.y},
+        };
+
+        draw_list->AddConvexPolyFilled(points, IM_ARRAYSIZE(points), IM_COL32(128, 64, 0, 255));
+      }
+
+      // Borders R-R
+      float pos_x = xy0.x;
+      float pos_y = xy0.y + h0;
       for (int r=0; r<storage.track_nums.size(); r++) {
         int track_id = storage.track_nums[r];
         DawTrack * track = storage.tracks[track_id].get();
 
         ImGui::PushID(r);
         pos_y += track->h;
-        HSplitter(pos_x, pos_y, avail.x, [&]() { track->h = ImMax(track->h + ImGui::GetIO().MouseDelta.y, 20.0f); });
+        HSplitter({pos_x, pos_y}, avail.x + 1500, [&]() { track->h = ImMax(track->h + ImGui::GetIO().MouseDelta.y, 20.0f); });
         ImGui::PopID();
         pos_y += 8;
       }
 
-      ImGui::SetCursorPos({1500, 1500});
+
       storage.scroll_y = ImGui::GetScrollY();
       storage.scroll_x1 = ImGui::GetScrollX();
     }
     ImGui::EndChild();
-
-    // ImGui::PopStyleVar();
+    ImGui::PopStyleColor();
+    ImGui::PopStyleVar(2);
   }
 
 
