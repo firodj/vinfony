0ad8fce1ff2c81dc189aaaac77db77db4dc7806e
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-05-19 09:35:42 +0700

    Add toolbar buttons

--- src/MainApp.cpp
+++ src/MainApp.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <mutex>
 #include <iostream>
+#define IMGUI_DEFINE_MATH_OPERATORS
+
 #include "imgui.h"
 #include "imgui_internal.h"
 #include "DawMain.hpp"
@@ -9,7 +11,7 @@
 #include <kosongg/INIReader.h>
 #include <ifd/ImFileDialog.hpp>
 #include <ifd/ImFileDialog_opengl.hpp>
-
+#include <kosongg/IconsFontAwesome6.h>
 static std::unique_ptr<MainApp> g_mainapp;
 
 static std::mutex g_mtxMainapp;
@@ -34,10 +36,79 @@ MainApp::MainApp(/* dependency */): kosongg::EngineBase(/* dependency */) {
 
 MainApp::~MainApp() {
 }
+const float toolbarSize = 50;
+float  menuBarHeight = 0;
+// https://github.com/tpecholt/imrad/blob/main/src/imrad.cpp
+void DockSpaceUI()
+{
+	ImGuiViewport* viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(viewport->Pos + ImVec2(0, toolbarSize));
+	ImGui::SetNextWindowSize(viewport->Size - ImVec2(0, toolbarSize));
+	ImGui::SetNextWindowViewport(viewport->ID);
+	ImGuiWindowFlags window_flags = 0
+		| ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking
+		| ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse
+		| ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove
+		| ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	ImGui::Begin("Master DockSpace", NULL, window_flags);
+	ImGuiID dockspace_id = ImGui::GetID("MyDockspace");
+
+	// Save off menu bar height for later.
+	menuBarHeight = ImGui::GetCurrentWindow()->MenuBarHeight();
+
+	ImGui::DockSpace(dockspace_id);
+
+  ImGui::DockBuilderAddNode(dockspace_id, ImGuiDockNodeFlags_PassthruCentralNode | ImGuiDockNodeFlags_DockSpace);
+	ImGui::DockBuilderSetNodeSize(dockspace_id, viewport->Size);
+
+
+  ImGui::DockBuilderFinish(dockspace_id);
+
+	ImGui::End();
+
+	ImGui::PopStyleVar(3);
+}
+
+void ToolbarUI()
+{
+	ImGuiViewport* viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(ImVec2(viewport->Pos.x, viewport->Pos.y + menuBarHeight));
+	ImGui::SetNextWindowSize(ImVec2(viewport->Size.x, toolbarSize));
+	ImGui::SetNextWindowViewport(viewport->ID);
+
+	ImGuiWindowFlags window_flags = 0
+		| ImGuiWindowFlags_NoDocking
+		| ImGuiWindowFlags_NoTitleBar
+		| ImGuiWindowFlags_NoResize
+		| ImGuiWindowFlags_NoMove
+		| ImGuiWindowFlags_NoScrollbar
+		| ImGuiWindowFlags_NoSavedSettings
+		;
+	ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+	ImGui::Begin("TOOLBAR", NULL, window_flags);
+	ImGui::PopStyleVar();
+
+  ImGui::PushStyleVar(ImGuiStyleVar_FrameRounding, 2);
+  ImGui::Button(ICON_FA_BACKWARD_FAST " Rewind");
+  ImGui::SameLine();
+	ImGui::Button(ICON_FA_PLAY " Play");
+  ImGui::SameLine();
+  ImGui::Button(ICON_FA_STOP " Stop");
+  ImGui::PopStyleVar();
+
+	ImGui::End();
+}
 
 void MainApp::RunImGui() {
   ImGuiIO& io = ImGui::GetIO(); (void)io;
   ImColor clearColor(m_clearColor);
+
+  DockSpaceUI();
+	ToolbarUI();
 
   // 1. Show the big demo window and another window
   EngineBase::RunImGui();
